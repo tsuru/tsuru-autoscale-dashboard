a0c35ab7071e72ead7c3fb85503884cf62b30879
Andrews Medina <andrewsmedina@gmail.com>
2015-05-06 14:26:51 -0300

    datasource/client: refactored tests.

--- datasource/tests.py
+++ datasource/tests.py
@@ -46,9 +46,15 @@ class DataSourceTestCase(TestCase):
 
 
 class ClientTestCase(TestCase):
+    def setUp(self):
+        httpretty.enable()
+
+    def tearDown(self):
+        httpretty.disable()
+        httpretty.reset()
+
     def test_new(self):
         os.environ["AUTOSCALE_HOST"] = "http://autoscalehost.com"
-        httpretty.enable()
         httpretty.register_uri(
             httpretty.POST,
             "http://autoscalehost.com/datasource",
@@ -56,18 +62,11 @@ class ClientTestCase(TestCase):
 
         client.new({})
 
-        httpretty.disable()
-        httpretty.reset()
-
     def test_list(self):
         os.environ["AUTOSCALE_HOST"] = "http://autoscalehost.com"
-        httpretty.enable()
         httpretty.register_uri(
             httpretty.GET,
             "http://autoscalehost.com/datasource",
         )
 
         client.list()
-
-        httpretty.disable()
-        httpretty.reset()
