26a267a38538322b0a1107d38012cf101e86bce4
Andrews Medina <andrewsmedina@gmail.com>
2015-05-11 23:30:03 -0300

    alarm: add link to remove in template list.

--- alarm/urls.py
+++ alarm/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 
 urlpatterns = [
+    url(r'^$', 'alarm.views.list', name='alarm-list'),
     url(r'^new/$', 'alarm.views.new', name='alarm-new'),
     url(r'^(?P<name>[\w\s-]+)/$', 'alarm.views.remove', name='alarm-remove'),
-    url(r'^$', 'alarm.views.list', name='alarm-list'),
 ]
