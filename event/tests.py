cc3408a7bc69e14be49ec1ff26104a40e5f7fdbf
Andrews Medina <andrewsmedina@gmail.com>
2015-05-20 22:53:23 -0300

    event: add client.

---
+++ event/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from event import client
+
+import httpretty
+
+import os
+
+
+class ClientTestCase(TestCase):
+    def setUp(self):
+        httpretty.enable()
+
+    def tearDown(self):
+        httpretty.disable()
+        httpretty.reset()
+
+    def test_list(self):
+        os.environ["AUTOSCALE_HOST"] = "http://autoscalehost.com"
+        httpretty.register_uri(
+            httpretty.GET,
+            "http://autoscalehost.com/alarm/abc/event",
+        )
+
+        client.list("abc", "token")
