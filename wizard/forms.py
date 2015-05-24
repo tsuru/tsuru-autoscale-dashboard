ba2244a16a8cf64cbf6085f6e7920bc69dd132b7
Andrews Medina <andrewsmedina@gmail.com>
2015-05-24 13:30:12 -0300

    wizard/forms: improve labels.

--- wizard/forms.py
+++ wizard/forms.py
@@ -1,13 +1,30 @@
 from django import forms
 
 
+OPERATOR_CHOICES = (
+    ('>', '>'),
+    ('>=', '>='),
+    ('<=', '<='),
+    ('<', '<'),
+    ('!=', '!='),
+)
+
+
+METRIC_CHOICES = (
+    ('cpu', 'cpu'),
+    ('memory', 'memory'),
+    ('request/min', 'request/min'),
+    ('response time', 'response time'),
+)
+
+
 class ScaleForm(forms.Form):
-    metric = forms.ChoiceField()
-    operator = forms.ChoiceField()
+    metric = forms.ChoiceField(METRIC_CHOICES)
+    operator = forms.ChoiceField(OPERATOR_CHOICES)
     value = forms.CharField()
-    units = forms.CharField()
-    wait = forms.IntegerField()
+    units = forms.CharField(label=u'Step (in units)')
+    wait = forms.IntegerField(label=u'Wait time (in seconds)')
 
 
 class ConfigForm(forms.Form):
-    min = forms.IntegerField()
+    min = forms.IntegerField(label=u'Start with units')
