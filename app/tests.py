2a5271289027f9f6d18e9a9a948a71bc20b0fe28
Andrews Medina <andrewsmedina@gmail.com>
2015-08-25 16:01:33 -0300

    app: add instance info on app-info

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+import mock
+
 
 class IndexTestCase(TestCase):
-    def test_index(self):
+    @mock.patch("instance.client.list")
+    def test_index(self, list_mock):
         url = "{}?TSURU_TOKEN=bla".format(reverse("app-info", args=["app"]))
         response = self.client.get(url)
         self.assertTemplateUsed(response, "app/index.html")
--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from instance import client
+
 
 def index(request, app):
-    return render(request, "app/index.html")
+    token = request.GET.get("TSURU_TOKEN")
+    instances = client.list(token).json() or []
+    instance = None
+    for inst in instances:
+        if app in inst.get('Apps', []):
+            instance = inst
+    context = {
+        "instance": instance,
+    }
+    return render(request, "app/index.html", context)
