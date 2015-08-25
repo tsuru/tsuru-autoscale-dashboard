2e5d0f42e820dbc58e242ce0e4d715bc7aedcc58
Andrews Medina <andrewsmedina@gmail.com>
2015-08-25 17:17:35 -0300

    app: add auto scale info by app

--- app/tests.py
+++ app/tests.py
@@ -10,3 +10,14 @@ class IndexTestCase(TestCase):
         url = "{}?TSURU_TOKEN=bla".format(reverse("app-info", args=["app"]))
         response = self.client.get(url)
         self.assertTemplateUsed(response, "app/index.html")
+
+    @mock.patch("instance.client.list")
+    def test_index_instance_not_found(self, list_mock):
+        response_mock = mock.Mock()
+        response_mock.json.return_value = None
+        list_mock.return_value = response_mock
+
+        url = "{}?TSURU_TOKEN=bla".format(reverse("app-info", args=["app"]))
+        response = self.client.get(url)
+
+        self.assertTemplateUsed(response, "app/index.html")
--- app/views.py
+++ app/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 
 from instance import client
+from wizard import client as wclient
 
 
 def index(request, app):
     token = request.GET.get("TSURU_TOKEN")
     instances = client.list(token).json() or []
+
     instance = None
+    auto_scale = None
+
     for inst in instances:
         if app in inst.get('Apps', []):
             instance = inst
+
+        response = wclient.get(instance["Name"], token)
+        if response.status_code == 200:
+            auto_scale = response.json()
+
     context = {
         "instance": instance,
+        "auto_scale": auto_scale,
     }
     return render(request, "app/index.html", context)
