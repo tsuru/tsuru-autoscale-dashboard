e7e07f2c330fa1618e110eba128f281923677ada
Andrews Medina <andrewsmedina@gmail.com>
2016-01-15 19:11:50 -0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from tsuru_autoscale import __version__
+
+
+setup(
+    name="tsuru_autoscale",
+    url="https://github.com/tsuru/tsuru-autoscale-dashboard",
+    version=__version__,
+    packages=find_packages(),
+    description="Web dashboard for tsuru autoscale service",
+    author="tsuru",
+    author_email="tsuru@corp.globo.com",
+    include_package_data=True,
+    install_requires=[
+        "Django>=1.9.1",
+        "requests>=2.8.1",
+    ],
+)
