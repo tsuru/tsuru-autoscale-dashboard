e5d33840cb959f42c0013a66353c5cdbeb9a64f3
Andrews Medina <andrewsmedina@gmail.com>
2016-08-03 15:34:07 -0300

    update Django

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email="tsuru@corp.globo.com",
     include_package_data=True,
     install_requires=[
-        "Django>=1.9.8",
+        "Django>=1.10",
         "requests>=2.8.1",
     ],
 )
